d2f75ab4197d0b578dfbcefdcfbeeb67c9a97298
Kiddishhh <75043723@qq.com>
2026-05-14 16:08:38 +0800

    chore(rag): update __init__.py exports for new modules

--- app/rag/__init__.py
+++ app/rag/__init__.py
@@ -1,3 +1,12 @@
 from .reranker import LLMReranker
+from .query_optimizer import QueryOptimizer, QueryOptimizeResult
+from .retriever import HybridRetriever
+from .text_splitter import ParentDocumentSplitter
 
-__all__ = ["LLMReranker"]
+__all__ = [
+    "LLMReranker",
+    "QueryOptimizer",
+    "QueryOptimizeResult",
+    "HybridRetriever",
+    "ParentDocumentSplitter",
+]
